5cf23755b134487481be1dd48af8bc73f0a02e47
reltaher <43248170+reltaher@users.noreply.github.com>
2019-12-16 17:41:23 -0500

    Added some images to the Portfolio section

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
     });
 
     var typed = new Typed(".typed", {
-        strings: ["Software Engineer.", "FGCU Student.", "Learning Assistant."],
+        strings: ["Software Engineer", "FGCU Student", "Learning Assistant"],
         typeSpeed: 70,
         loop: true,
         startDelay: 1000,
